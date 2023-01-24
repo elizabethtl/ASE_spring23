d062dc9741750dd983cb58d8de64ad795fe02efb
wutever0017 <elizabethlin77@gmail.com>
2023-01-24 12:36:27 -0500

    hw1 test

--- hw1/src/hw1.py
+++ hw1/src/hw1.py
@@ -161,34 +161,34 @@ def eg(key, str, fun):
 
 ##################
 ### test functions
-def test_the():
-  oo(the)
+# def test_the():
+#   oo(the)
 
-def test_rand():
-  num1 = Num()
-  num2 = Num()
+# def test_rand():
+#   num1 = Num()
+#   num2 = Num()
 
-  for i in range(1, 1000):
-    num1.add(rand(0, 1))
-  for i in range(1, 1000):
-    num2.add(rand(0, 1))
-  m1 = rnd(num1.mid(), 10)
-  # print(m1)
-  m2 = rnd(num2.mid(), 10)
-  # print(m2)
-  return m1==m2 and 0.5==rnd(m1, 1)
+#   for i in range(1, 1000):
+#     num1.add(rand(0, 1))
+#   for i in range(1, 1000):
+#     num2.add(rand(0, 1))
+#   m1 = rnd(num1.mid(), 10)
+#   # print(m1)
+#   m2 = rnd(num2.mid(), 10)
+#   # print(m2)
+#   return m1==m2 and 0.5==rnd(m1, 1)
 
-def test_sym():
-  sym = Sym()
-  for x in ['a', 'a', 'a', 'a', 'b', 'b', 'c']:
-    sym.add(x)
-  return 'a'==sym.mid() and 1.379==rnd(sym.div())
+# def test_sym():
+#   sym = Sym()
+#   for x in ['a', 'a', 'a', 'a', 'b', 'b', 'c']:
+#     sym.add(x)
+#   return 'a'==sym.mid() and 1.379==rnd(sym.div())
 
-def test_num():
-  num = Num()
-  for x in [1, 1, 1, 1, 2, 2, 3]:
-    num.add(x)
-  return 11/7==num.mid() and 0.787==rnd(num.div())
+# def test_num():
+#   num = Num()
+#   for x in [1, 1, 1, 1, 2, 2, 3]:
+#     num.add(x)
+#   return 11/7==num.mid() and 0.787==rnd(num.div())
 
 #######
 ### cli
@@ -236,44 +236,41 @@ def coerce(s):
 
 #########
 ### main
-def main():
-  saved = {}
-  fails = 0
+# def main():
+#   saved = {}
+#   fails = 0
 
-  for k, v in cli(settings(help)).items():
-    # print(k, v)
-    the[k] = v
-    saved[k] = v
+#   for k, v in cli(settings(help)).items():
+#     # print(k, v)
+#     the[k] = v
+#     saved[k] = v
 
-  if the['help'] == True:
-    print(help)
+#   if the['help'] == True:
+#     print(help)
 
-  
+#   else:
+#     # run test functions
+#     for what, fun in egs.items():
+#       # print(what, fun)
 
-  else:
+#       if the['go'] == 'all' or what == the['go']:
+#         for k, v in saved.items():
+#           the[k] = v
+#           Seed = the['seed']
 
-    # run test functions
-    for what, fun in egs.items():
-      # print(what, fun)
-
-      if the['go'] == 'all' or what == the['go']:
-        for k, v in saved.items():
-          the[k] = v
-          Seed = the['seed']
-
-        if egs[what]() == False:
-          fails += 1
-          print("❌ fail:", what)
-        else:
-          print("✅ pass:", what)
+#         if egs[what]() == False:
+#           fails += 1
+#           print("❌ fail:", what)
+#         else:
+#           print("✅ pass:", what)
 
 
 
-if __name__ == '__main__':
-  eg("the", "show settings", test_the)
-  eg("rand", "generate, reset, regenerate same", test_rand)
-  eg("sym", "check syms", test_sym)
-  eg("num", "check nums", test_num)
-  main()
+# if __name__ == '__main__':
+#   eg("the", "show settings", test_the)
+#   eg("rand", "generate, reset, regenerate same", test_rand)
+#   eg("sym", "check syms", test_sym)
+#   eg("num", "check nums", test_num)
+#   main()
 
   
---
+++ hw1/test/test_hw1.py
@@ -0,0 +1,66 @@
+import pytest
+from src.hw1 import *
+
+def test_the():
+  oo(the)
+
+def test_rand():
+  num1 = Num()
+  num2 = Num()
+
+  for i in range(1, 1000):
+    num1.add(rand(0, 1))
+  for i in range(1, 1000):
+    num2.add(rand(0, 1))
+  m1 = rnd(num1.mid(), 10)
+  # print(m1)
+  m2 = rnd(num2.mid(), 10)
+  # print(m2)
+  return m1==m2 and 0.5==rnd(m1, 1)
+
+def test_sym():
+  sym = Sym()
+  for x in ['a', 'a', 'a', 'a', 'b', 'b', 'c']:
+    sym.add(x)
+  return 'a'==sym.mid() and 1.379==rnd(sym.div())
+
+def test_num():
+  num = Num()
+  for x in [1, 1, 1, 1, 2, 2, 3]:
+    num.add(x)
+  return 11/7==num.mid() and 0.787==rnd(num.div())
+
+def main():
+
+  eg("the", "show settings", test_the)
+  eg("rand", "generate, reset, regenerate same", test_rand)
+  eg("sym", "check syms", test_sym)
+  eg("num", "check nums", test_num)
+
+
+  saved = {}
+  fails = 0
+
+  for k, v in cli(settings(help)).items():
+    # print(k, v)
+    the[k] = v
+    saved[k] = v
+
+  if the['help'] == True:
+    print(help)
+
+  else:
+    # run test functions
+    for what, fun in egs.items():
+      # print(what, fun)
+
+      if the['go'] == 'all' or what == the['go']:
+        for k, v in saved.items():
+          the[k] = v
+          Seed = the['seed']
+
+        if egs[what]() == False:
+          fails += 1
+          print("❌ fail:", what)
+        else:
+          print("✅ pass:", what)
